91cbbb396658f2b96ef71ae2b6e6590bc85c567c
Natalia1324 <natalia.omiotek15@gmail.com>
2024-04-11 18:29:17 +0200

    new project

---
+++ Hellven/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private bool isMoving;
+
+    private Vector2 input;
+
+    private Animator animator;
+
+    public LayerMask solidLayer;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (!isMoving)
+        {
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+            
+            Debug.Log(input.x + " " + input.y);
+
+
+            if (input != Vector2.zero)
+            {
+                animator.SetFloat("MoveX", input.x);
+                animator.SetFloat("MoveY", input.y);
+
+                var targetPos = transform.position;
+                targetPos.x += input.x;
+                targetPos.y += input.y;
+
+                if (isWalkable(targetPos)) StartCoroutine(Move(targetPos));
+            }
+        }
+
+        animator.SetBool("isMoving", isMoving);
+    }
+
+    IEnumerator Move(Vector3 targetPos)
+    {
+        isMoving = true;
+        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = targetPos;
+
+        isMoving = false;
+    }
+
+    private bool isWalkable(Vector3 targetPos)
+    {
+        if (Physics2D.OverlapCircle(targetPos, 0.01f, solidLayer) != null)
+        {
+            return false;
+        }
+        return true;
+
+    }
+}
