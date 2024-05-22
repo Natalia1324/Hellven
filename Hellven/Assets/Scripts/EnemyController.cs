47ceb38ca73695b783774e9c681af01bbc182af4
Natalia1324 <natalia.omiotek15@gmail.com>
2024-05-22 15:58:11 +0200

    animacja walki, jakiś interfejs, swiecenie przeciwnika na czerwono podczas damage, efekty cząsteczkowe

---
+++ Hellven/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class EnemyController : MonoBehaviour
+{
+    public int maxHealth = 20;
+    private int currentHealth;
+    [SerializeField] private ParticleSystem test = default(ParticleSystem);
+    public float hitColorDuration = 0.5f; // Czas trwania zmiany koloru po trafieniu
+    public Color hitColor = Color.red; // Kolor trafienia
+
+    private SpriteRenderer spriteRenderer;
+    void Start()
+    {
+        currentHealth = maxHealth;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+        else
+        {
+            StartCoroutine(FlashEnemyColor());
+        }
+    }
+    IEnumerator FlashEnemyColor()
+    {
+        // Zapisz pierwotny kolor
+        Color originalColor = spriteRenderer.color;
+
+        // Zmień kolor na trafienie
+        spriteRenderer.color = hitColor;
+
+        // Poczekaj przez hitColorDuration sekund
+        yield return new WaitForSeconds(hitColorDuration);
+
+        // Przywróć pierwotny kolor
+        spriteRenderer.color = originalColor;
+    }
+    void Die()
+    {
+        Debug.Log("Enemy died!");
+        ParticleSystem newParticleSystem = Instantiate(test);
+        // Ustaw pozycję nowego systemu cząsteczek na pozycję tego obiektu
+        newParticleSystem.transform.position = transform.position;
+        newParticleSystem.gameObject.layer = 0;
+        var particleRenderer = newParticleSystem.GetComponent<Renderer>();
+        particleRenderer.sortingOrder = 1;
+        // Uruchom animację cząsteczek
+        newParticleSystem.Play();
+        // Zniszcz nowo utworzony system cząsteczek po zakończeniu animacji
+        Destroy(newParticleSystem.gameObject, newParticleSystem.main.duration);
+        Destroy(gameObject);
+    }
+}
--- Hellven/Assets/Scripts/PlayerController.cs
+++ Hellven/Assets/Scripts/PlayerController.cs
@@ -1,26 +1,50 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public int maxHP;
+    public int currentHP;
     public float moveSpeed;
 
     private bool isMoving;
+    //public Transform attackPoint;
+    //public float attackRange = 0.5f;
+    //public LayerMask enemyLayers;
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+    public int attackDamage = 10;
+    public float attackDelay = 0.5f;
+
+
+    private bool isAttacking;
 
     private Vector2 input;
 
     private Animator animator;
 
     public LayerMask solidLayer;
-    public LayerMask battleLayer;
+    //public LayerMask battleLayer;
 
-
+    public TextMeshProUGUI healthText;
     private void Awake()
     {
         animator = GetComponent<Animator>();
+        
     }
-
+    private void Start()
+    {
+        //currentHP = maxHP;
+        UpdateHealthUI();
+    }
+    void UpdateHealthUI()
+    {
+        Debug.Log("HP is: " +  currentHP);
+        healthText.text = "HP: " + currentHP + "/" + maxHP;
+    }
     private void Update()
     {
         if (!isMoving)
@@ -28,7 +52,7 @@ public class PlayerController : MonoBehaviour
             input.x = Input.GetAxisRaw("Horizontal");
             input.y = Input.GetAxisRaw("Vertical");
             
-            Debug.Log(input.x + " " + input.y);
+            //Debug.Log(input.x + " " + input.y);
 
 
             if (input != Vector2.zero)
@@ -43,9 +67,87 @@ public class PlayerController : MonoBehaviour
                 if (isWalkable(targetPos)) StartCoroutine(Move(targetPos));
             }
         }
-
+        if (Input.GetKey(KeyCode.Space))
+        {
+            StartCoroutine(Attack());
+        }
+        else isAttacking = false;
+        //Debug.Log("Attack is: "+ isAttacking);
+        animator.SetBool("isAttacking", isAttacking);
         animator.SetBool("isMoving", isMoving);
     }
+
+    //void Attack()
+    //{
+    //    Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+    //    // Zadanie obrażeń trafionym przeciwnikom
+    //    foreach (Collider2D enemy in hitEnemies)
+    //    {
+    //        Debug.Log("Trafiono: " + enemy.name);
+    //        // Tutaj dodaj kod zadawania obrażeń przeciwnikom
+    //    }
+    //}
+
+    IEnumerator Attack()
+    {
+        isAttacking = true;
+
+        // Włączenie animacji ataku
+        //animator.SetTrigger("Attack");
+
+        // Opóźnienie przed zadaniem obrażeń
+        yield return new WaitForSeconds(attackDelay);
+
+        // Sprawdzenie kolizji z przeciwnikami
+        //Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        //// Zadanie obrażeń trafionym przeciwnikom
+        //foreach (Collider2D enemy in hitEnemies)
+        //{
+        //    Debug.Log("Trafiono: " + enemy.name);
+        //    enemy.GetComponent<EnemyController>().TakeDamage(attackDamage);
+        //}
+
+        isAttacking = false;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHP -= damage;
+        if (currentHP <= 0)
+        {
+            Die();
+        }
+        UpdateHealthUI();
+    }
+
+    void Die()
+    {
+        Debug.Log("Player died!");
+        // Tutaj można dodać logikę śmierci gracza
+    }
+    public void DealDamage()
+    {
+        // Sprawdzenie kolizji z przeciwnikami
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        // Zadanie obrażeń trafionym przeciwnikom
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("Trafiono: " + enemy.name);
+            enemy.GetComponent<EnemyController>().TakeDamage(attackDamage);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+
 
     IEnumerator Move(Vector3 targetPos)
     {
