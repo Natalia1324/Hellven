68d4b944466d2888701f1fbe32067b5602fb3ccf
GryczonJ <92317770+GryczonJ@users.noreply.github.com>
2024-05-27 20:40:39 +0200

    Działa
    dodawanie itmów
    
    Co-Authored-By: Kanra <149872920+Kanra11@users.noreply.github.com>

--- Hellven/Assets/Scripts/Eq/db/ItemsDB.cs
+++ Hellven/Assets/Scripts/Eq/db/ItemsDB.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Item;
 
 public class ItemsDB : MonoBehaviour
 {
@@ -18,7 +19,28 @@ public class ItemsDB : MonoBehaviour
 
     }
     public void StartsItem() {
-        //InGame.Add(new Clothes(-5,,"Okulary","Nosisz na g³owie",1,"Staki d³ugi",5));
+        //InGame.Add(new Clothes(4, "Assets/Cainos/Pixel Art Icon Pack - RPG/Texture/Potion/Blue Potion.png", "Okulary","Nosisz na g³owie",1,"Staki d³ugi",5));
+
+    }
+    public struct pomoc {
+        public ItemType Type;
+
+        public int Id;
+        public Texture2D Tekstura;
+        public string Name;
+        public string Description;
+        public int Quantity;
+        public bool Eatable;
+        public pomoc(int Id, Texture2D texture, string name, string Description, int quantity, ItemType type, bool eatable)
+        {
+            this.Id = Id;
+            this.Tekstura = texture;
+            this.Name = name;
+            this.Description = Description;
+            this.Quantity = quantity;
+            this.Type = type;
+            this.Eatable = eatable;
+        }
     }
 
 }
