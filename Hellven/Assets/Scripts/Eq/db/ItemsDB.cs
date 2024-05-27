88e558114cbabf8a18117ea3bcb0c013b0f0027e
GryczonJ <92317770+GryczonJ@users.noreply.github.com>
2024-05-27 22:00:57 +0200

    eq
    Co-Authored-By: Kanra <149872920+Kanra11@users.noreply.github.com>

--- Hellven/Assets/Scripts/Eq/db/ItemsDB.cs
+++ Hellven/Assets/Scripts/Eq/db/ItemsDB.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using static Item;
+using static UnityEngine.Terrain;
 
 public class ItemsDB : MonoBehaviour
 {
@@ -18,9 +19,29 @@ public class ItemsDB : MonoBehaviour
     {
 
     }
+
+    public void addItemPlayer(int Id, Texture2D texture, string name, string Description, int quantity, ItemType type, bool eatable)
+    {
+        Player.Add(new Item(Id, texture, name, Description, quantity, type, eatable));
+    }
+    public void addFoodPlayer(int Id, Texture2D texture, string name, string Description, int quantity, int calories, bool eatable)
+    {
+        Player.Add(new Food(Id, texture, name, Description, quantity, calories, eatable));
+    }
+    public void addWeaponPlayer(int Id, Texture2D texture, string name, string Description, int quantity, int damage, bool eatable)
+    {
+        Player.Add(new Weapon(Id, texture, name, Description, quantity, damage, eatable));
+    }
+    public void addMaterialsPlayer(int Id, Texture2D texture, string name, string Description, int quantity, string materialType, bool eatable)
+    {
+        Player.Add(new Materials(Id, texture, name, Description, quantity, materialType, eatable));
+    }
+    public void addClothesPlayer(int Id, Texture2D texture, string name, string Description, int quantity, string size, int defence, bool eatable)
+    {
+        Player.Add(new Clothes(Id, texture, name, Description, quantity, size, defence, eatable));
+    }
     public void StartsItem() {
-        //InGame.Add(new Clothes(4, "Assets/Cainos/Pixel Art Icon Pack - RPG/Texture/Potion/Blue Potion.png", "Okulary","Nosisz na g³owie",1,"Staki d³ugi",5));
-        //InGame.Add(new Item(4, "Assets/Cainos/Pixel Art Icon Pack - RPG/Texture/Potion/Blue Potion.png", "Okulary", "Nosisz na g³owie", 1, "Staki d³ugi", 5));
+        
     }
     //public struct pomoc {
     //    public ItemType Type;
